3d083909096cf223239385977757d8137aeab428
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-10-02 18:03:00 +0900

    cleanup

--- mlgym/images/classify.py
+++ mlgym/images/classify.py
@@ -2,8 +2,6 @@ import numpy as np
 import chainer
 import chainer.functions as F
 import chainer.links as L
-from chainer import training
-from chainer.training import extensions
 
 import dagen
 import dagen.image
@@ -15,19 +13,17 @@ params = {}
 params["batch_size"] = 8
 
 X_train, Y_train = get_ds_simple(cnt_samples=1000)
-#X_train = X_train.astype(np.float32)[:,np.newaxis]
 X_train = np.expand_dims(X_train, axis=1).astype(np.float32) / 255
-Y_train = Y_train[:,np.newaxis]
+Y_train = Y_train[:, np.newaxis]
 print(X_train.shape)
 print(Y_train.shape)
-
 
 
 class Net(chainer.Chain):
 
     def __init__(self, train=True):
         super(Net, self).__init__(
-            conv1=L.Convolution2D(1, 32, 2), #Convolution2D(in_channels, out_channels, ksize, stride=1, pad=0, wscale=1, bias=0, nobias=False, use_cudnn=True, initialW=None, initial_bias=None, deterministic=False)
+            conv1=L.Convolution2D(1, 32, 2),
             conv2=L.Convolution2D(None, 32, 2),
             l1=L.Linear(None, 10),
             l2=L.Linear(None, 1)
@@ -42,6 +38,7 @@ class Net(chainer.Chain):
         h = self.l2(h)
         return h
 
+
 class Classifier(chainer.Chain):
     def __init__(self, predictor):
         super(Classifier, self).__init__(predictor=predictor)
@@ -53,25 +50,9 @@ class Classifier(chainer.Chain):
         chainer.report({'loss': loss, 'accuracy': accuracy}, self)
         return loss
 
-model = Classifier(Net())
-
-#def train():
-    #nb_epoch = 20
-    #optimizer = chainer.optimizers.Adam()
-    #optimizer.setup(model)
-    #train = chainer.datasets.tuple_dataset.TupleDataset(X_train, Y_train)
-    #train_iter = chainer.iterators.SerialIterator(train, batch_size=params["batch_size"], repeat=True, shuffle=False)
-    #updater = training.StandardUpdater(train_iter, optimizer)
-    #trainer = training.Trainer(updater, (nb_epoch, 'epoch'), out='/tmp/result')
-    ## trainer.extend(extensions.Evaluator(test_iter, model, device=id_device))
-    ## trainer.extend(extensions.Evaluator(test_iter, model))
-    #trainer.extend(extensions.LogReport())
-    #trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', "elapsed_time"]))
-    ## trainer.extend(extensions.ProgressBar())
-    #trainer.run()
-
 
 def main():
+    model = Classifier(Net())
     ds_train = chainer.datasets.tuple_dataset.TupleDataset(X_train, Y_train)
     train(model, ds_train)
 
